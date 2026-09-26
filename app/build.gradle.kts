162e4209b320173c0ef2804afaa3dd62f374a6f6
akibul1735 <145787639+akibul1735@users.noreply.github.com>
2026-09-26 08:37:22 +0600

    Hh

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,10 @@ android {
         compose = true
         buildConfig = true
     }
+    lint {
+        abortOnError = false
+        checkReleaseBuilds = false
+    }
     testOptions {
         unitTests.isIncludeAndroidResources = true
     }
